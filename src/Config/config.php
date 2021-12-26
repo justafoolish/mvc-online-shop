a3e777efeea2a10ca14c55a10d893d5a52b3402b
Hắc Tún <hactuns@macbookpro.local>
2021-12-26 22:22:16 +0700

    test production/heroku

--- src/Config/config.php
+++ src/Config/config.php
@@ -1,8 +1,16 @@
 <?php
-define("BASE_URL", "http://localhost/mvcphp");
+// define("BASE_URL", "http://localhost/mvcphp");
 
-define('HOST', 'localhost');
-define('USERNAME', 'root');
-define('PASSWD', '');
-define('DB_NAME', 'cnpm_db');
+// define('HOST', 'localhost');
+// define('USERNAME', 'root');
+// define('PASSWD', '');
+// define('DB_NAME', 'cnpm_db');
 
+/** Product **/
+
+define("BASE_URL", "https://clone-hades.herokuapp.com/");
+
+define('HOST', 'remotemysql.com:3306');
+define('USERNAME', 'V05To2dybm');
+define('PASSWD', '5Oe8r68oR2');
+define('DB_NAME', 'V05To2dybm');
